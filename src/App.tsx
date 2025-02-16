3823b055d789d9237000b03dfaabd318364bf507
Vinicio Brejinski <viniciosdb@gmail.com>
2025-02-15 22:15:33 -0300

    styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { ThemeProvider } from 'styled-components'
 import { Button } from './components/Button'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
-    <div>
+    <ThemeProvider theme={defaultTheme} >
       <h1>Hollo World</h1>
       <Button variant="primary" />
       <Button variant="secondery" />
       <Button variant="danger" />
       <Button variant="success" />
       <Button />
-    </div>
+    </ThemeProvider>
   )
 }
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 export type ButtonVariant = 'primary' | 'secondery' | 'danger' | 'success'
 
@@ -6,19 +6,13 @@ interface ButtonContainerProps {
   variant: ButtonVariant
 }
 
-const buttonVariant = {
-  primary: 'purple',
-  secondery: 'orange',
-  danger: 'red',
-  success: 'green',
-}
-
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-  ${({ variant }) =>
-    css`
-      background: ${buttonVariant[variant]};
-    `}
+  background-color: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.white};
 `
